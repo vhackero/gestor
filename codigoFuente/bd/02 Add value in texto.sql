8287412defbaced72572d122e3afefedb2a2870b
Alejandro Toscuento Flores <38266367+dark2829@users.noreply.github.com>
2023-12-13 10:40:12 -0600

    add script for insert text in insignias

---
+++ codigoFuente/bd/02 Add value in texto.sql
@@ -0,0 +1,5 @@
+INSERT INTO 
+des_sisi_gestor.tbl_textos_sistema 
+(clave, valor, id_funcionalidad, fecha_registro, fecha_actualizacion, usuario_modifico) 
+VALUES 
+('gw.badge.editar.exito', 'Insignia editada correctamente', '1', '2023-12-07 17:49:40', '2023-12-07 17:49:40', '2');
