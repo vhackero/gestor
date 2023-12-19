e1445315a0a78ec8f5bf76ba067d9d669f9ab8fe
ChrisRojano08 <crisrojano8@gmail.com>
2023-12-19 00:04:42 -0600

    [ChrisRojano08] Fix bad query for insert tbl_organismos_gubernamentales

--- codigoFuente/bd/05 scripts for catalogs.sql
+++ codigoFuente/bd/05 scripts for catalogs.sql
@@ -5,7 +5,7 @@ UPDATE des_sisi_gestor.tbl_organismos_gubernamentales SET nombre = 'UnADM' WHERE
 UPDATE des_sisi_gestor.tbl_organismos_gubernamentales SET nombre = 'ITESM' WHERE (id = '125');
 # -- Inserts necesarios para la tabla -- 
 INSERT INTO des_sisi_gestor.tbl_organismos_gubernamentales 
-("nombre", "id_padre", "id_tipo_organismo", "fecha_registro", "usuario_modifico", "fecha_actualizacion", "activo") 
+(nombre, id_padre, id_tipo_organismo, fecha_registro, usuario_modifico, fecha_actualizacion, activo) 
 VALUES 
 ('UNAM', '1', '2', '2023-12-14 18:11:46', '2', '2023-12-14 18:11:46', '1'), 
 ('IPN', '1', '2', '2023-12-14 18:11:46', '2', '2023-12-14 18:11:46', '1'),
