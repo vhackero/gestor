a28e3388f16a8381d6d5788e80db7bbbe7cef17c
ChrisRojano08 <crisrojano8@gmail.com>
2024-01-02 17:24:33 -0600

    [ChrisRojano08] Fixed some errors

---
+++ codigoFuente/bd/17_AddMissingOptionForCompetencies.sql
@@ -0,0 +1,7 @@
+INSERT INTO `cat_competencias_plan` (`id`, `nombre`, `descripcion`, `activo`, `orden`, `fecha_registro`, `fecha_actualizacion`, `usuario_modifico`) VALUES
+('2', 'Opcional', '', '1', '1', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1);
+
+INSERT INTO `cat_tipo_evento_ec` (`id`, `nombre`, `descripcion`, `fecha_registro`, `activo`, `orden`, `fecha_actualizacion`, `usuario_modifico`) VALUES
+(4, 'Materia', '', CURRENT_TIMESTAMP, '1', '1', CURRENT_TIMESTAMP, '1'),
+(5, 'Asignatura', '', CURRENT_TIMESTAMP, '1', '1', CURRENT_TIMESTAMP, '1');
+
