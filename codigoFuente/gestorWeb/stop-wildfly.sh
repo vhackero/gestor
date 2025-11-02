6adee66d69a9cdb5197630155108523fecde1881
Juan Manuel Munoz <macuco8@hotmail.com>
2025-11-01 18:13:36 -0600

    archivos que no se ocupan

--- codigoFuente/gestorWeb/stop-wildfly.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export JAVA_HOME="/Users/macuco/Library/Java/JavaVirtualMachines/temurin-1.8.0_442/Contents/Home"
-export PATH="$JAVA_HOME/bin:$PATH"
-
-JBOSS_CLI="/Users/macuco/Documents/proyectos/t4g/gestor/wildfly-10.1.0.Final/bin/jboss-cli.sh"
-
-"$JBOSS_CLI" --connect --command=":shutdown"
