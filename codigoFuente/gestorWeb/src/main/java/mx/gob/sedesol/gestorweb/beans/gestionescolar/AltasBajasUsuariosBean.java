013ed52eda0b62721ab972fbeb526ccac9ea829c
macuco <macuco8@hotmail.com>
2025-11-12 18:36:20 -0600

    Implementa flujo interactivo de altas y bajas

--- codigoFuente/gestorWeb/src/main/java/mx/gob/sedesol/gestorweb/beans/gestionescolar/AltasBajasUsuariosBean.java
+++ codigoFuente/gestorWeb/src/main/java/mx/gob/sedesol/gestorweb/beans/gestionescolar/AltasBajasUsuariosBean.java
@@ -17,34 +17,47 @@ public class AltasBajasUsuariosBean extends BaseBean {
     private static final Logger LOGGER = Logger.getLogger(AltasBajasUsuariosBean.class);
 
     private String paginaActual;
+    private boolean mostrarOpcionesBajas;
 
     @PostConstruct
     public void init() {
         LOGGER.info("Inicializando módulo de altas y bajas de usuarios");
         paginaActual = null;
+        mostrarOpcionesBajas = false;
     }
 
     public String irNuevaAlta() {
         LOGGER.info("Navegando a la vista de nueva alta de usuario");
         this.paginaActual = "/views/private/gestionEscolar/altasBajasUsuarios/nuevaAlta.xhtml";
+        this.mostrarOpcionesBajas = false;
         return null;
     }
 
     public String irNuevaBaja() {
         LOGGER.info("Navegando a la vista de nueva baja de usuario");
         this.paginaActual = "/views/private/gestionEscolar/altasBajasUsuarios/nuevaBaja.xhtml";
+        this.mostrarOpcionesBajas = true;
         return null;
     }
 
     public String irConsultarBaja() {
         LOGGER.info("Navegando a la vista de consulta de bajas de usuario");
         this.paginaActual = "/views/private/gestionEscolar/altasBajasUsuarios/consultaBaja.xhtml";
+        this.mostrarOpcionesBajas = true;
+        return null;
+    }
+
+    public String mostrarOpcionesBajas() {
+        LOGGER.info("Mostrando opciones de bajas de usuarios");
+        this.mostrarOpcionesBajas = true;
+        this.paginaActual = null;
         return null;
     }
 
     public String regresarAlModulo() {
         LOGGER.info("Regresando al módulo de altas y bajas de usuarios");
         this.paginaActual = null;
+        this.mostrarOpcionesBajas = false;
         return null;
     }
 
@@ -54,5 +67,13 @@ public class AltasBajasUsuariosBean extends BaseBean {
 
     public void setPaginaActual(String paginaActual) {
         this.paginaActual = paginaActual;
+    }
+
+    public boolean isMostrarOpcionesBajas() {
+        return mostrarOpcionesBajas;
+    }
+
+    public void setMostrarOpcionesBajas(boolean mostrarOpcionesBajas) {
+        this.mostrarOpcionesBajas = mostrarOpcionesBajas;
     }
 }
