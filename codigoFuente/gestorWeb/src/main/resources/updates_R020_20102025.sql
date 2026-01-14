feeb6e5cf56e441d97f91b1c02353bf9d2133115
Sunflower-S14 <0.atcsn@gmail.com>
2026-01-14 01:55:26 -0600

    Se agregan los scripts SQL para modificar la tabla cat_fuentes_externas

--- codigoFuente/gestorWeb/src/main/resources/updates_R020_20102025.sql
+++ codigoFuente/gestorWeb/src/main/resources/updates_R020_20102025.sql
@@ -48,6 +48,20 @@ INSERT INTO des_sisi_gestor.tbl_textos_sistema
        FROM des_sisi_gestor.tbl_funcionalidades f
        WHERE f.clave = 'ALT_BAJ_USR';
 
+-- Modificación a la tabla cat_fuentes_externas
+ALTER TABLE `cat_fuentes_externas`
+       ADD `servidor` VARCHAR(255) NOT NULL AFTER `fecha_registro`,
+       ADD `contrasena` VARCHAR(255) AFTER `servidor`,
+       ADD `usuario` VARCHAR(255) AFTER `contrasena`,
+       ADD `alias` VARCHAR(255) AFTER `usuario`,
+       ADD `nombre_base_datos` VARCHAR(255) AFTER `alias`,
+       ADD `consulta` VARCHAR(600) AFTER `nombre_base_datos`;
+
+-- Ejemplo inserción de datos en las nuevas columnas
+UPDATE `cat_fuentes_externas` SET `usuario` = 'root' WHERE `cat_fuentes_externas`.`id_fuente_externa` = 1;
+UPDATE `cat_fuentes_externas` SET `alias` = 'localhost' WHERE `cat_fuentes_externas`.`id_fuente_externa` = 1;
+UPDATE `cat_fuentes_externas` SET `consulta` = 'SELECT user AS matricula_sige, password AS password_sige, name AS nombre_sige, apellido_paterno AS apellidop_sige, apellido_materno AS  apellidom_sige, programa AS programa_educativo_sige, division AS division_sige, email AS correo_institucional_sige, fecha_nacimiento AS fecha_nacimiento_sige, curp AS curp_sige, nivel AS nivel_sige, persona_id AS persona_id_sige, perfil_id  AS perfil_id_sige FROM users WHERE user = ? ' WHERE `cat_fuentes_externas`.`id_fuente_externa` = 1;
+
 
 -- Modales del formulario Importar fuentes externas
 
